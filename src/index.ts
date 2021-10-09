26c718c0fc51a7f7281dc0b2e0c8830c25b3df1d
uztbt <yujitabata@icloud.com>
2021-10-10 00:09:11 +0900

    Parse arguments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers';
+import express from 'express';
+
+yargs(hideBin(process.argv))
+  .command(
+    'serve [port]',
+    'start the server',
+    yargs => yargs.positional('port', {
+      describe: 'port to bind on',
+      default: 5000
+    }),
+    argv => {
+      // const app = express();
+      console.log(`hello world, argv.port = ${argv.port}, argv.usCentralUrl = ${argv.usCentralUrl}`);
+    })
+  .option('usCentralUrl', {
+    alias: 'us-central-url',
+    description: 'URL of the node in the us-central region'
+  })
+  .demandOption(['usCentralUrl'], 'Please provide the urls of your JaPong nodes.')
+  .help()
+  .parse();
